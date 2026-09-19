2347d5c83890599575b2bdf9187417cf51ca5e4c
Fenriz <kastonbag552@gmail.com>
2026-09-19 20:07:13 +0300

    555

--- Content.Server/GameTicking/ServerGameTicker.Spawning.cs
+++ Content.Server/GameTicking/ServerGameTicker.Spawning.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Numerics;
 using Content.Server.Administration.Managers;
 using Content.Server.Administration.Systems;
-using Content.Server.Database;
 using Content.Server.GameTicking.Events;
 using Content.Shared.CCVar;
 using Content.Shared.Database;
@@ -18,7 +17,6 @@ using Content.Shared.Roles;
 using Content.Shared.Roles.Jobs;
 using Content.Shared.Spawners.Components;
 using Content.Shared.Station.Components;
-using NetCord;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Network;
@@ -26,8 +24,6 @@ using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
-using Serilog;
-using Color = Robust.Shared.Maths.Color;
 
 namespace Content.Server.GameTicking
 {
